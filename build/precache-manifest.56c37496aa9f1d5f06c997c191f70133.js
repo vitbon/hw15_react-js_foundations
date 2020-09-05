11ad088837fc6ac82333ba0adcaa538e42baf4af
Vitalii <vit.bondarchuk@gmail.com>
2020-09-05 21:21:53 +0300

    HW15 deploy

---
+++ build/precache-manifest.56c37496aa9f1d5f06c997c191f70133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d70d146a5b76e6ae366e196377a1745",
+    "url": "/cursor-homeworks/index.html"
+  },
+  {
+    "revision": "db72fe6e57a3b7a1324a",
+    "url": "/cursor-homeworks/static/css/main.215bdc97.chunk.css"
+  },
+  {
+    "revision": "1f4a261234b7773e03d9",
+    "url": "/cursor-homeworks/static/js/2.181a29a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-homeworks/static/js/2.181a29a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db72fe6e57a3b7a1324a",
+    "url": "/cursor-homeworks/static/js/main.86a67f0e.chunk.js"
+  },
+  {
+    "revision": "d848f14a336f420e054e",
+    "url": "/cursor-homeworks/static/js/runtime-main.6efce928.js"
+  }
+]);
