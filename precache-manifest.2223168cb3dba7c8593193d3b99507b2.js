0e93bb5bddb1504907b124c467479a7d0ba44030
Vitalii <vit.bondarchuk@gmail.com>
2020-09-05 21:22:26 +0300

    Updates

---
+++ precache-manifest.2223168cb3dba7c8593193d3b99507b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f354e7d5c15491d30e51873972d77",
+    "url": "/hw15_react-js_foundations/index.html"
+  },
+  {
+    "revision": "db72fe6e57a3b7a1324a",
+    "url": "/hw15_react-js_foundations/static/css/main.215bdc97.chunk.css"
+  },
+  {
+    "revision": "1f4a261234b7773e03d9",
+    "url": "/hw15_react-js_foundations/static/js/2.181a29a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw15_react-js_foundations/static/js/2.181a29a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db72fe6e57a3b7a1324a",
+    "url": "/hw15_react-js_foundations/static/js/main.86a67f0e.chunk.js"
+  },
+  {
+    "revision": "0bc443f3681711f3f416",
+    "url": "/hw15_react-js_foundations/static/js/runtime-main.9e251503.js"
+  }
+]);
